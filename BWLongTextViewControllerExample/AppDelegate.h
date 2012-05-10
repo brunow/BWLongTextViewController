7650d4701bfb8c1d97f1059a177253b60c25ca25
Bruno Wernimont <bruno.wernimont@gmail.com>
2012-05-10 22:28:44 +0200

    First import

---
+++ BWLongTextViewControllerExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BWLongTextViewControllerExample
+//
+//  Created by Bruno Wernimont on 10/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
